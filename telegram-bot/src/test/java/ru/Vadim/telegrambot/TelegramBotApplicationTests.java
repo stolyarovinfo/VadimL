c95ff58bfcf85dac83b7a947f925183d42332de8
Vadim <vadim.lobzenko.03@mail.ru>
2023-07-06 16:42:47 +0300

    Practical task 2. Created page with Hello World

---
+++ telegram-bot/src/test/java/ru/Vadim/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Vadim.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
